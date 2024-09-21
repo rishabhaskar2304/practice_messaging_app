faa180299f61bfcc291981de62b4c8462eb11792
rishabhaskar2304 <rishabhaskar.2304@gmail.com>
2024-09-22 00:48:23 +0530

    Using manager actor for userActor interactions

--- src/main/java/com/bms/messaging/MessagingApplication.java
+++ src/main/java/com/bms/messaging/MessagingApplication.java
@@ -2,26 +2,51 @@ package com.bms.messaging;
 
 import akka.actor.typed.ActorRef;
 import akka.actor.typed.ActorSystem;
-import akka.actor.typed.Props;
-import com.bms.messaging.actor.UserActor;
+import akka.actor.typed.javadsl.AskPattern;
+import akka.actor.typed.receptionist.Receptionist;
+import akka.actor.typed.receptionist.ServiceKey;
+import com.bms.messaging.actor.ManagerActor;
 import com.bms.messaging.entity.Message;
+import com.bms.messaging.entity.RegisterUser;
 import com.bms.messaging.entity.SendMessage;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.time.Duration;
+
+import java.util.concurrent.CompletionStage;
 
 //@SpringBootApplication
 public class MessagingApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 //		SpringApplication.run(MessagingApplication.class, args);
-		ActorSystem<Message> system = ActorSystem.create(UserActor.create("system"), "system");
+		ActorSystem<Message> system = ActorSystem.create(ManagerActor.create(), "Manager");
 
-		ActorRef<Message> actor1 = system.systemActorOf(UserActor.create("actor1"), "actor1", Props.empty());
-		system.systemActorOf(UserActor.create("actor2"), "actor2", Props.empty());
+		system.tell(new RegisterUser("actor1"));
+		system.tell(new RegisterUser("actor2"));
 
-		SendMessage message = new SendMessage("actor1", "actor2", "Hello there, actor2");
-		actor1.tell(message);
+		Thread.sleep(1000);
+
+		ServiceKey<Message> userServiceKey = ServiceKey.create(Message.class, "actor1");
+
+		CompletionStage<Receptionist.Listing> listingFuture = AskPattern.ask(
+				system.receptionist(),
+				replyTo -> Receptionist.find(userServiceKey, replyTo),
+				Duration.ofSeconds(3),
+				system.scheduler()
+		);
+
+		listingFuture.thenAccept(listing -> {
+			if (!listing.getServiceInstances(userServiceKey).isEmpty()) {
+				ActorRef<Message> actor1 = listing.getServiceInstances(userServiceKey).iterator().next();
+
+				SendMessage message = new SendMessage("actor2", "actor1", "Hello there, actor1");
+				actor1.tell(message);
+			} else {
+				System.out.println("actor1 not found");
+			}
+		});
 	}
 
 }
